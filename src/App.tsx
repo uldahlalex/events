4676a7eeb6aa08cf78f6c1768457f505ca0513e2
Alex Uldahl Pedersen <alex@uldahl.dk>
2025-08-12 14:24:50 +0200

    form

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from 'react'
+
+export interface RegisterModel {
+    email: string;
+    password: string;
+    repeatPassword: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [registerModel, setregisterModel] = useState<RegisterModel>({
+      email: '',
+      password: '',
+      repeatPassword: ''
+  })
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input type="email" value={registerModel.email} onChange={e => setregisterModel({...registerModel, email: e.target.value})} placeholder="email" />
+      <input type="password" value={registerModel.password} onChange={e => setregisterModel({...registerModel, password: e.target.value})} placeholder="password" />
+      <input type="password" value={registerModel.repeatPassword} onChange={e => setregisterModel({...registerModel, repeatPassword: e.target.value})} placeholder="repeat" />
+        <button onClick={() => {
+            if(registerModel.password.length < 6) {
+                alert('too short pass')
+                return;
+            }
+            if(registerModel.password != registerModel.repeatPassword) {
+                alert("dont match")
+                return;
+            }
+            alert('success')
+        }}>register</button>
     </>
   )
 }
